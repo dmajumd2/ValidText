e3358432c49474b43e78ae934ae13bbc8a1d165e
Debashish <dmajumd2@binghamton.edu>
2018-06-03 12:35:15 -0400

    Added validation for removing html when value filled in textbox

--- JavaScript-Ass1.js
+++ JavaScript-Ass1.js
@@ -14,7 +14,6 @@ var domStrings = {
     buttonRight: '.btnRight'
 };
 
-var clicks = 0;
 var arrayText1;
 var arrayText2;
 var clicks = 0;
@@ -24,57 +23,108 @@ document.querySelector(domStrings.save).addEventListener('click', function(){
        var h = document.getElementById("uname");
         clicks += 1;
         if(clicks <= 1){
-            h.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please enter the username</span></p>");
+            h.insertAdjacentHTML("afterend", "<p id='un'><span style='color:red'>Please enter the username</span></p>");
             clicks--;
         }
     }
 
+     if(document.getElementById('uname').value && document.getElementById('un')){
+        document.getElementById("un").outerHTML = "";
+     }
+
     if(document.querySelector(domStrings.password).value === ""){
-        console.log('asdas')
         var h1 = document.getElementById("pwd");
-        console.log(h1);
           clicks += 1;
           if(clicks <= 1){
-             h1.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please enter the password</span></p>");
+             h1.insertAdjacentHTML("afterend", "<p id = 'pd'><span style='color:red'>Please enter the password</span></p>");
              clicks--;
          }
+     } 
+     if(document.getElementById('pwd').value && document.getElementById('pd')){
+        document.getElementById("pd").outerHTML = "";
      }
 
      if(document.querySelector(domStrings.email).value === ""){
         var h = document.getElementById("email");
          clicks += 1;
          if(clicks <= 1){
-             h.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please enter the email</span></p>");
+             h.insertAdjacentHTML("afterend", "<p id='em'><span style='color:red'>Please enter the email</span></p>");
              clicks--;
          }
-     }  
+     }
+     if(document.getElementById('email').value && document.getElementById('em')){
+        document.getElementById("em").outerHTML = "";
+     }
+       
 
      if(document.querySelector(domStrings.location).value === ""){
         var h = document.getElementById("loc");
          clicks += 1;
          if(clicks <= 1){
-             h.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please enter the location</span></p>");
+             h.insertAdjacentHTML("afterend", "<p id='lo'><span style='color:red'>Please enter the location</span></p>");
              clicks--;
          }
-     }  
+     } 
+     if(document.getElementById('loc').value && document.getElementById('lo')){
+        document.getElementById("lo").outerHTML = "";
+     } 
 
      if(document.querySelector(domStrings.company).value === ""){
         var h = document.getElementById("comp");
          clicks += 1;
          if(clicks <= 1){
-             h.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please enter the company</span></p>");
+             h.insertAdjacentHTML("afterend", "<p id='co'><span style='color:red'>Please enter the company</span></p>");
              clicks--;
          }
+     } 
+     if(document.getElementById('comp').value && document.getElementById('co')){
+        document.getElementById("co").outerHTML = "";
      }
      
      if(document.getElementById("male").checked != true && document.getElementById("female").checked != true){
         var h = document.getElementById("gender");
          clicks += 1;
          if(clicks <= 1){
-             h.insertAdjacentHTML("afterend", "<p><span style='color:red'>Please select gender</span></p>");
+             h.insertAdjacentHTML("afterend", "<p id='ge'><span style='color:red'>Please select gender</span></p>");
              clicks--;
          }
      }
+     if((document.getElementById('male').checked || document.getElementById('female').checked)  && document.getElementById('ge')){
+        document.getElementById("ge").outerHTML = "";
+     }
+     clicks++;
+     clicks++;
+
+     var users = [];
+     if(localStorage.a_users) {
+        users = JSON.parse(localStorage.a_users);
+        //displayUsers(users);
+    }
+
+     
+        var gender;
+        if(document.getElementById("male").checked == true){
+            console.log('male');
+            gender = 'male';
+        }
+        if(document.getElementById("female").checked == true){
+            console.log('female');
+            gender = 'female';
+        }
+    
+        var user = {
+            'username':document.getElementById('uname').value,
+            'password':document.getElementById('pwd').value,
+            'email': document.getElementById('email').value,
+            'location': document.getElementById('loc').value,
+            'company': document.getElementById('comp').val1,
+            'gender': gender
+            
+            
+        };
+        users.push(user);
+        localStorage.a_users = JSON.stringify(users);
+  
 });
 
 
@@ -120,7 +170,7 @@ document.querySelector(domStrings.buttonRight).addEventListener('click', functio
         setTimeout(function(){
             document.getElementById("text1").value += number;
             arrayText2.splice(0,1);
-            console.log(arrayText2);
+            //console.log(arrayText2);
             document.getElementById("text2").value = arrayText2;
             }, 2000 + offset1);    
        offset1 += 2000;
